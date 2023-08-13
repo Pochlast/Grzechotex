d5505eb695bf2cafd4c022bba21874bf8f4fe628
Pochlast <121972229+Pochlast@users.noreply.github.com>
2023-08-13 22:03:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ class Ball{
         this.elasticity = 1;
         this.vel = new Vector(0,0);
         this.acc = new Vector(0,0);
-        this.acceleration = 1;
+        this.acceleration = 3;
         this.player = true;
         BALLZ.push(this);
     }
