1cc4dd530774c66cb1d540c1b08b26f89bedb6ca
Mrowka333 <39970211+MrsMrowka@users.noreply.github.com>
2023-08-27 23:03:51 +0200

    default rattle css

--- script.js
+++ script.js
@@ -17,8 +17,6 @@ function startRattle() {
     let quantity = document.querySelector("#quantity").value;
     let size = document.querySelector("#one").checked;
 
-    console.log(quantity);
-    console.log(size);
     document.querySelector(".home-view").style["display"] = "none";
     document.querySelector(".rattle-view").style["display"] = "block";
 
